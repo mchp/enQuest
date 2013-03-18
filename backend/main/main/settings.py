91fa5f90a411f2a15cd7fdeb28fd9fe60cfbcbde
Nick Adams <nwadams@nickadams.ca>
2013-03-17 23:31:07 -0700

    Update settings.py
    change db user/password

--- backend/main/main/settings.py
+++ backend/main/main/settings.py
@@ -14,8 +14,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'enQuest',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'root',
-        'PASSWORD': 'root',
+        'USER': 'enQuest',
+        'PASSWORD': 'x7HFjzPMGjxxP86y',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
